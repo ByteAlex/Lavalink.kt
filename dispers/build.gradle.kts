94829e4ee288fe2b61a74549b3e94d10db6a2f95
ByteAlex <abecker@bitflow.dev>
2022-09-27 17:03:46 +0200

    update dep

--- dispers/build.gradle.kts
+++ dispers/build.gradle.kts
@@ -21,6 +21,12 @@ repositories {
 
 kotlin {
     sourceSets {
+        commonMain {
+            dependencies {
+                api(projects.core)
+            }
+        }
+
         commonTest {
             dependencies {
                 implementation(kotlin("test-common"))
